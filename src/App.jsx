bb62a1d62639a1a291d4c8550f37d7d78d8e3e9c
Nishant Saini <161240067+nishant21saini@users.noreply.github.com>
2025-07-02 22:55:12 +0530

    feat: initialize project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-900">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
